8640fca26babe94ada81e5bff346d7a0b2d05e8d
unknown <mandari.louaye@gmail.com>
2023-07-26 15:24:03 +0300

    second commit

---
+++ src/components/auth/auth/Login.js
@@ -0,0 +1,28 @@
+import { Button } from '@nextui-org/react'
+import React from 'react'
+
+function Login() {
+    const loginbar = 'border border-gray-300 focus:border-blue-500 my-2 focus:outline-none px-4 py-2 rounded-md'
+    const buttonbar = 'twitterBlue border rounded-3xl text-slate-200 w-3/4 mx-auto h-[54px]  '
+  return (
+    <div className="flex flex-row">
+    {/* Background Image Div */}
+    <div className="login-background flex-1">
+      {/* You can add any content inside this div */}
+      hi
+    </div>
+
+    {/* Form Content Div */}
+    <div className='flex flex-col my-6 mx-12 flex-1'>
+        <h1 className='font-bold text-3xl'>Log in to Twitter</h1>
+        <input placeholder='Email' className={loginbar} type="text" />
+        <input placeholder='password' className={loginbar} type="password" />
+        
+        <button className={buttonbar}>Login</button>
+
+        </div>
+    </div>
+  )
+}
+
+export default Login
