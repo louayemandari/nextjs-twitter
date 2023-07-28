099ff07e3a3ea4ed6779ff22dfabfbd5b522c488
unknown <mandari.louaye@gmail.com>
2023-07-29 02:46:57 +0300

    just fixed the search bar UI

--- src/components/rightSide/Search.js
+++ src/components/rightSide/Search.js
@@ -2,9 +2,9 @@ import React from 'react'
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 function Search() {
   return (
-    <div className='mx-1 my-2 rounded-full flex bg-slate-500 mr-10'>
+    <div className='mx-1 my-2 rounded-full flex bg-slate-500 '>
       <SearchOutlinedIcon className='text-white m-1'/>
-      <input type="text" className='bg-slate-500 '  />
+      <input type="text" className='bg-slate-500 rounded-full w-[120px] focus:outline-none text-slate-100 '  />
     </div>
   )
 }
